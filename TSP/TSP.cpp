46f6b4ec8f2289d89f947e29702292b5c44f9383
Sage Oltmanns <soltmanns@omnitech-inc.com>
2022-10-07 10:47:08 -0600

    Initial commit

---
+++ TSP/TSP.cpp
@@ -0,0 +1,18 @@
+/**************************
+    Authors: Noah Johnson and Sage Oltmanns
+    Date: October 26th, 2022
+    Description:
+        Add description of the traveling salesman problem
+
+    TODO:
+        Task 1 - Temporary Task
+**************************/
+
+#include <iostream>
+using namespace std;
+
+int main()
+{
+    cout << "Hello World!\n";
+    return 0;
+}
