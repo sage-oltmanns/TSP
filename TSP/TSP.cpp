506b3648c9a05e1f93a623e1bc2b64a91b21a503
Sage Oltmanns <soltmanns@omnitech-inc.com>
2022-10-24 12:45:38 -0600

    Everything should be working. I added comments. I will probably do documentation as well.

--- TSP/TSP.cpp
+++ TSP/TSP.cpp
@@ -1,49 +1,123 @@
 #include <iostream>
 #include <fstream>
 #include <vector>
+#include <string>
 using namespace std;
 
 struct Point {
 	float x, y;
 };
 
-float distance(int i, int j, vector<Point> a)
+struct bests {
+	float best_distance;
+	string best_path;
+};
+
+string correctBestPath(string best_path)
+{
+	size_t num_cities = best_path.length();
+	
+	// Fix best path values since indexes are 1 off 
+	int i;
+	for (i = 0; i < num_cities; i++)
+	{
+		best_path[i] = int(best_path[i]) - 1;
+	}
+	
+	return best_path;
+}
+
+float distance(int i, int j, vector<Point> cities)
 {
 	return sqrt(
-		(a[i].x - a[j].x) * (a[i].x - a[j].x)
-		+ (a[i].y - a[j].y) * (a[i].y - a[j].y));
+		(cities[i].x - cities[j].x) * (cities[i].x - cities[j].x)
+		+ (cities[i].y - cities[j].y) * (cities[i].y - cities[j].y));
 }
 
 
-float findTourDistance(int i, int j, vector<vector<float>>& dp, vector<Point> a)
+bests findTourDistance(int i, int j, vector<vector<bests>>& dp, vector<Point> cities)
 {
-	if (dp[i][j] > 0) {
+	// If a distance is already in the table return it
+	if (dp[i][j].best_distance > 0) 
 		return dp[i][j];
-	}
 
-	float distance1 = findTourDistance(i + 1, j, dp, a) + distance(i, i + 1, a);
-	float distance2 = findTourDistance(i + 1, i, dp, a) + distance(j, i + 1, a);
+	// Recursive call to find tour distance to add points to a tour
+	float distance1 = findTourDistance(i + 1, j, dp, cities).best_distance + distance(i, i + 1, cities);
+	float distance2 = findTourDistance(i + 1, i, dp, cities).best_distance + distance(j, i + 1, cities);
 	
 	if (distance1 > distance2) {
-		dp[i][j] = distance2;
+		// Check if point j is in the path already
+		size_t temp = dp[i + 1][i].best_path.find(to_string(j));
+
+		// If it in the list remove it
+		if (temp != -1) 
+			dp[i + 1][i].best_path.replace(temp, 1, "");
+		
+		// Add j to the end of the chosen best path
+		dp[i][j].best_path = dp[i + 1][i].best_path + to_string(j);
+
+		// Add the best distance to the point into the dp table
+		dp[i][j].best_distance = distance2;
 	}
 	else {
-		dp[i][j] = distance1;
+		// Check if point i is in the path already
+		size_t temp = dp[i + 1][j].best_path.find(to_string(i));
+
+		// If it is remove it
+		if (temp != -1)
+			dp[i + 1][j].best_path.replace(temp, 1, "");
+	
+		// Add i to the end of the chosen best path
+		dp[i][j].best_path = dp[i + 1][j].best_path + to_string(i);
+
+		// Add the best distance to the dp table
+		dp[i][j].best_distance = distance1;
+		
 	}
 
 	return dp[i][j];
 }
 
-
-
-void TSP(int N, vector<Point> a)
+vector<Point> getCities(ifstream& fin, int numberOfCities)
 {
-	vector<vector<float>> dp(N, vector<float> (N, 0));
+	vector<Point> cities(numberOfCities + 1);
 
+	// Read in cities from input file
+	int i;
+	for (i = 1; i <= numberOfCities; i++)
+	{
+		fin >> cities[i].x;
+		fin >> cities[i].y;
+	}
+
+	return cities;
+}
+
+void outputSolution(ofstream& fout, bests solution)
+{
+	size_t num_cities = solution.best_path.length();
+	// Output cities backwards since we find them 
+	int i;
+	for (i = int(num_cities) - 1; i >= 0; i--)
+	{
+		fout << solution.best_path[i] << "  ";
+	}
+	fout << endl << solution.best_distance;
+}
+
+bests TSP(int N, vector<Point> cities)
+{
+	vector<vector<bests>> dp(N, vector<bests>(N, { 0, "" }));
+
+	// Initialize paths to the rightmost point
 	for (int j = 1; j < N - 1; j++)
-		dp[N - 1][j] = distance(N - 1, N, a)
-		+ distance(j, N, a);
-	cout << findTourDistance(1, 1, dp, a) << endl;
+	{
+		dp[N - 1][j].best_distance = distance(N - 1, N, cities)
+		+ distance(j, N, cities);
+		dp[N - 1][j].best_path += to_string(j) + to_string(N) +  to_string(N - 1);
+	}
+
+	return findTourDistance(1, 1, dp, cities);
 }
 
 
@@ -51,31 +125,44 @@ void TSP(int N, vector<Point> a)
 int main(int argc, char** argv)
 {
 	ifstream fin;
+	ofstream fout;
 	int numberOfCities;
 	if (argc != 2) {
-		cout << "Missing a Sample File" << endl;
+		cout << "Missing an Input File" << endl;
+		cout << "Args: tsp.exe inputfile.in" << endl;
 		return -1;
 	}
+	string inputFile = argv[1];
 
-	fin.open(argv[1]);
+	size_t extensionLocation = inputFile.find_last_of(".");
+	string outputFile = inputFile.substr(0, extensionLocation) + ".out";
 
-	if (!fin) {
-		cout << "File could not be opened" << endl;
+	fin.open(inputFile);
+	fout.open(outputFile);
+
+	if (!fin) 
+	{
+		cout << "Input file could not be opened" << endl;
+		cout << "Args: tsp.exe inputfile.in" << endl;
 		return -2;
 	}
+	if (!fout) 
+	{
+		cout << "Output file could not be opened" << endl;
+		return -3;
+	}
+
 
 	fin >> numberOfCities;
-	vector<Point> a(numberOfCities + 1);
-	int i;
-	for (i = 1; i <= numberOfCities; i++) 
-	{
-		fin >> a[i].x;
-		fin >> a[i].y;
-	}
-	
 
-	TSP(numberOfCities, a);
+	vector<Point> cities = getCities(fin, numberOfCities);
 
+	bests solution = TSP(numberOfCities, cities);
+
+	solution.best_path = correctBestPath(solution.best_path);
+
+	outputSolution(fout, solution);
 	fin.close();
+	fout.close();
 	return 0;
 }
